e31771beb537e48d2655ba4f653533db26f0ee49
mpkelevra23 <mpkelevra23@gmail.com>
2019-02-25 16:04:18 +0300

    lesson_3

--- controllers/actions/ActivityCreateAction.php
+++ controllers/actions/ActivityCreateAction.php
@@ -11,14 +11,16 @@ namespace app\controllers\actions;
 use app\components\ActivityComponent;
 use app\models\Activity;
 use yii\base\Action;
+use yii\web\UploadedFile;
 
 class ActivityCreateAction extends Action
 {
     public function run()
     {
+        /** @var ActivityComponent $comp */
         $comp = \Yii::createObject([
             'class' => ActivityComponent::class,
-                'activity_class' => Activity::class
+            'activity_class' => Activity::class
         ]);
 
         if (\Yii::$app->request->isPost) {
@@ -27,10 +29,20 @@ class ActivityCreateAction extends Action
              */
             $comp = \Yii::createObject([
                 'class' => ActivityComponent::class,
-//                'activity_class' => Activity::class
+                'activity_class' => Activity::class
             ]);
-            $activity = $comp->getModel(\Yii::$app->request->post());
-            $comp->createActivity($activity);
+            if ($activity = $comp->getModel(\Yii::$app->request->post())) {
+                $activity->files = UploadedFile::getInstances($activity, 'files');
+                if ($activity->files) {
+                    foreach ($activity->files as $file) {
+                        $file->saveAs('data/' . $file->baseName . '.' . $file->extension);
+                    }
+                }
+            }
+
+            if ($comp->createActivity($activity)) {
+                return $this->controller->render('show', ['activity' => $activity]);
+            }
 
 
         } else {
